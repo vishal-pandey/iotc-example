861b672d714d212d92763510d3cd8f31c2017512
VISHAL PANDEY <contact@vishalpandey.xyz>
2020-06-23 13:42:07 +0530

    Testing App

--- app/src/main/java/com/softwaremakeinindia/iotcexample/MainActivity.java
+++ app/src/main/java/com/softwaremakeinindia/iotcexample/MainActivity.java
@@ -4,16 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.softwaremakeinindia.iotc.Iotc;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList myList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.activity_listview, myList);
+        ListView listView = findViewById(R.id.message_list);
+        listView.setAdapter(adapter);
+
+        final EditText eText = findViewById(R.id.edittext);
+        Button btn = findViewById(R.id.button);
+        Button clearBtn = findViewById(R.id.clearBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = eText.getText().toString();
+                Iotc.send("device",msg);
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myList.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
 
         String[] devicesToSubscribe = {"device"};
         Iotc.connect(MainActivity.this, "key", devicesToSubscribe, new Iotc.OnConnect() {
@@ -21,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onMessageReceive(String deviceId, String msg) {
-                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onMessageReceive: message"+ msg);
+                myList.add(msg+" from "+deviceId);
+                adapter.notifyDataSetChanged();
             }
         });
     }
